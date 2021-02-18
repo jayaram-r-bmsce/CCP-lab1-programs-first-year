c27aa2b7864ec59443e4cfb41095ce06eb177841
jayaram_r <78743957+jayaram-r-bmsce@users.noreply.github.com>
2021-02-18 10:19:37 +0530

    Update root of quadratic equation.c

--- root of quadratic equation.c
+++ root of quadratic equation.c
@@ -1,21 +1,25 @@
 #include <stdio.h>
 #include <conio.h>
 #include <math.h>
-int main() {
+int main() 
+{
     float a, b, c, discriminant, root1, root2, realPart, imagPart;
     printf("enter the three numbers \n");
     scanf("%f %f %f", &a, &b, &c);
     discriminant = pow(b,2) - 4 * a * c;
-    if (discriminant > 0) {
+    if (discriminant > 0) 
+    {
         root1 = (-b + sqrt(discriminant)) / (2 * a);
         root2 = (-b - sqrt(discriminant)) / (2 * a);
         printf("root1 = %f and root2 = %f", root1, root2);
     }
-    else if (discriminant == 0) {
+    else if (discriminant == 0) 
+    {
         root1 = root2 = -b / (2 * a);
         printf("root1 = root2 = %f", root1);
     }
-    else {
+    else 
+    {
         realPart = -b / (2 * a);
         imagPart = sqrt(-discriminant) / (2 * a);
         printf("1st root = (%f + %f i) and 2nd root = (%f - %f i) , where i = (-1)^(0.5)", realPart, imagPart, realPart, imagPart);
